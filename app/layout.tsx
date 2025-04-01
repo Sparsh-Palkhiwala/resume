0d54a165919ae5436fdae58a0f94410cf49b08e1
Sparsh Palkhiwala <106943650+Sparsh-Palkhiwala@users.noreply.github.com>
2025-03-31 23:47:06 -0700

    Write my first blog and add resume link

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Sparsh Devang Palkhiwala',
+    template: '%s | Sparsh Portfolio',
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
+    title: 'Sparsh Devang Palkhiwala',
     description: 'This is my portfolio.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Sparsh Devang Palkhiwala',
     locale: 'en_US',
     type: 'website',
   },
